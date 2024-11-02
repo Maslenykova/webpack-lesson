f16363ab9968342250db99f369d6614edf9339b0
Maslenykova <smaslenykova8@gmail.com>
2024-11-02 18:45:19 +0200

    lesson1/task1

---
+++ lesson1/task1/dist/scripts/logger/logger.js
@@ -0,0 +1,15 @@
+export const createLogger = (name) =>{
+    const logs = [];
+
+    return {
+        log(message){
+            logs.push(`log - ${name} - ${message}`);
+        },
+        error(errorText){
+            logs.push(`log - ${name} - ${errorText}`);
+        },
+        getLogs(){
+            return logs;
+        }
+    }
+}
