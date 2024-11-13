8ff7ed91b67476de054c892c022af77d53c2c274
Maslenykova <smaslenykova8@gmail.com>
2024-11-13 09:42:26 +0200

    lesson9/task1

--- lesson9/task1/.eslintrc.js
+++ lesson9/task1/.eslintrc.js
@@ -1,17 +1,10 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
+    extends: "eslint-config-airbnb-base",
+    rules: {
+        'no-console': 1,
     },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
+    env:{
+        browser: true,
     }
+   
 };
